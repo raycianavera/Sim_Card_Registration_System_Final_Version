1ed37f964b36da0324ea3f855c0a127e9ef59363
Wendell <wendell.anorma@gmail.com>
2022-04-03 23:06:56 +0800

    enduser

--- dbh/Admin_Seller.inc.php
+++ dbh/Admin_Seller.inc.php
@@ -1,9 +1,21 @@
 <?php
-//.inc.php. .inc has nothing to do. just naming purpose
-$dbServername = "us-cdbr-east-05.cleardb.net";
-$dbUsername = "bb9fcd6313deff"; //this will be ONLY change if using server not local
-$dbPassword = "8c9f15bf"; //i set a password to nothing . you can put one
-$dbName = "heroku_c1df3a5b9bfc854"; //this is where we will put our database name
+$servername = "us-cdbr-east-05.cleardb.net";
+$dbUsername = "bb9fcd6313deff";
+$dbpassword = "8c9f15bf";
+// name of database
+$dbName= "heroku_c1df3a5b9bfc854m";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword,$dbName) //stands for CONNection
- ?>
+ini_set ('error_reporting', E_ALL);
+ini_set ('display_errors', '1');
+error_reporting (E_ALL|E_STRICT);
+
+$db = mysqli_init();
+mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+$db->ssl_set('/etc/mysql/ssl/client-key.pem', '/etc/mysql/ssl/client-cert.pem', '/etc/mysql/ssl/ca-cert.pem', NULL, NULL);
+$conn = mysqli_real_connect ($db, $servername, $dbUsername, $dbpassword, $dbName, 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$conn)
+{
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+} 
+?>
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,14 +1,21 @@
 <?php
-
 $servername = "us-cdbr-east-05.cleardb.net";
 $dbUsername = "bb9fcd6313deff";
 $dbpassword = "8c9f15bf";
 // name of database
-$dbName= "heroku_c1df3a5b9bfc854";
+$dbName= "heroku_c1df3a5b9bfc854m";
 
-// CREATE CONNECTION (DATABASE)
-$conn = mysqli_connect($servername, $dbUsername,$dbpassword,$dbName);
+ini_set ('error_reporting', E_ALL);
+ini_set ('display_errors', '1');
+error_reporting (E_ALL|E_STRICT);
 
-if(!$conn){
-  die("Connection failed:" . mysqli_connect_error());
-}
+$db = mysqli_init();
+mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+$db->ssl_set('/etc/mysql/ssl/client-key.pem', '/etc/mysql/ssl/client-cert.pem', '/etc/mysql/ssl/ca-cert.pem', NULL, NULL);
+$conn = mysqli_real_connect ($db, $servername, $dbUsername, $dbpassword, $dbName, 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$conn)
+{
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+} 
+?>
