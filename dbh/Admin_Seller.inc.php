a85e3ceef778978749433df8336203ffc8bbd353
Wendell <wendell.anorma@gmail.com>
2022-04-03 23:26:53 +0800

    updated connection

--- dbh/Admin_Seller.inc.php
+++ dbh/Admin_Seller.inc.php
@@ -1,21 +1,14 @@
 <?php
-$servername = "us-cdbr-east-05.cleardb.net";
-$dbUsername = "bb9fcd6313deff";
-$dbpassword = "8c9f15bf";
-// name of database
-$dbName= "heroku_c1df3a5b9bfc854m";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-ini_set ('error_reporting', E_ALL);
-ini_set ('display_errors', '1');
-error_reporting (E_ALL|E_STRICT);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-$db = mysqli_init();
-mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+$conn = new mysqli($server, $username, $password, $db);
 
-$db->ssl_set('/etc/mysql/ssl/client-key.pem', '/etc/mysql/ssl/client-cert.pem', '/etc/mysql/ssl/ca-cert.pem', NULL, NULL);
-$conn = mysqli_real_connect ($db, $servername, $dbUsername, $dbpassword, $dbName, 3306, NULL, MYSQLI_CLIENT_SSL);
-if (!$conn)
-{
-    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
-} 
+if(!$conn){
+  die("Connection failed:" . mysqli_connect_error());
+}
 ?>
