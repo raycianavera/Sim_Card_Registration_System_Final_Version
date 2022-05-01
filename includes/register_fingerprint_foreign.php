0c20d1d8f48c79360ab098408bd131932c100577
JasminDuenas <minduenas11@gmail.com>
2022-05-01 20:24:31 +0800

    remove comments: register_fingerprint_foreign.php and register_fingerprint.php

--- includes/register_fingerprint_foreign.php
+++ includes/register_fingerprint_foreign.php
@@ -63,8 +63,6 @@ if(isset($_POST['register'])){
          $resultsCheck = mysqli_num_rows($result);
          if($resultsCheck == 1){
        header("Location: ../register-users-foreign.php?error=simnum-already-exist");
-       // echo "<script> window.location.href='../register-users-foreign.php?error=simnum-already-exist'; </script>";
-       // echo "<h2>Error</h2>";
      }
 
      else {
@@ -77,8 +75,7 @@ if(isset($_POST['register'])){
          echo "SQL statement failed";
        }else{
          //enter image error handlers
-         //////////////////////  IMAGE ERRORS  /////////////////////
-           if($fileSize==0){   //ERROR 404 for no file added
+           if($fileSize==0){
              header("Location: ../register-users-foreign.php?imageempty");
              exit();
            }else{
@@ -98,7 +95,7 @@ if(isset($_POST['register'])){
                      exit();
                    }
                    //enter mobile number error handlers
-                   //////////////////////  MOBILE NUMBER ERRORS  /////////////////////
+
                      $noplusnum = str_replace("+","",$simnum); //remove "+"
                      if(preg_match("/^[a-zA-Z_ -]*$/", $noplusnum)){ // ERROR 404 for not being number
                          header("Location: ../register-users-foreign.php?error=wrongchars");
@@ -117,9 +114,9 @@ if(isset($_POST['register'])){
                      // RUN PARAMETER INDSIDE DATABASE
                      mysqli_stmt_execute($stmt);
                      $result = mysqli_stmt_get_result($stmt);
-                     $fileDestination = '../Fingerprint_Registered_User_Database/'.$Fingerprint_ImageFullName; //kung saan move yung fingerprint sa folder. dapat same yung folder name. ikaw na bahala
-                     move_uploaded_file($fileTempName,$fileDestination);  //imomove na yung file to that folder
-                     // echo "<script> window.location.href='../register-users-foreign.php?signup=success'; </script>";
+                     $fileDestination = '../Fingerprint_Registered_User_Database/'.$Fingerprint_ImageFullName;
+                     move_uploaded_file($fileTempName,$fileDestination);
+
                      header("Location: ../register-users-foreign.php?signup=success");
                    }
                  }
